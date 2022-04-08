c07c1e2a4b739b796aafce5c34d80b228d3958ff
Ali Ahmad <turgut@Alis-MacBook-Pro.local>
2022-04-08 23:42:57 +0300

    view folder functions completed, html files completed

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,11 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+        "my_list":[ 
+            {"res1":"food_type1"},
+            {"res2":"food_type2"},
+            {"res3":"food_type3"}
+         ]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +18,6 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+        "my_object":{"res1":"food_type1"}
     }
     return render(request, 'detail.html', context)
